c00d5432006fa9f399420febec3540f6c50d98a7
AmenaChowdhury <amchdata@gmail.com>
2019-03-06 18:35:27 -0500

    restApiNaveen

---
+++ restassuredbddframework/src/test/java/com/qa/rest/tests/GetCallBDD.java
@@ -0,0 +1,32 @@
+
+package com.qa.rest.tests;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+public class GetCallBDD {
+	
+	@Test
+	public void test_numberof() {
+		
+		//given()
+		//when()
+		//then()
+		//assert()
+		given().
+		when().
+		get("http://ergast.com/api/f1/2017/circuits.json").
+		then(). 
+		assertThat().
+		
+		statusCode(200).
+		
+		and().
+		
+		body("MRData.CircuitTable.Circuits.CircuitId", hasSize(20)).
+		
+		and().
+		header("Content-Length", equalTo("4551"));
+	}
+
+}
